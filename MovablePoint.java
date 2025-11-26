06c1a101df2ff760429fb41ffd817ab1e41674b7
Bao25CE <trinhbao140@gmail.com>
2025-11-26 21:57:30 +0700

    6.3

--- MovablePoint.java
+++ MovablePoint.java
@@ -1,50 +1,28 @@
-public class MovablePoint extends Point {
-    private float xSpeed = 0.0f;
-    private float ySpeed = 0.0f;
+public class MovablePoint {
+    int x;
+    int y;
+    int xSpeed;
+    int ySpeed;
 
-    public MovablePoint(float x, float y, float xSpeed, float ySpeed) {
-        super(x, y);
+    public MovablePoint(int y, int x, int xSpeed, int ySpeed) {
+        this.y = y;
+        this.x = x;
         this.xSpeed = xSpeed;
         this.ySpeed = ySpeed;
     }
-    public MovablePoint(float xSpeed, float ySpeed){
-        this.xSpeed = xSpeed;
-        this.ySpeed = ySpeed;
+    public String toString(){
+        return "("+x+","+y+") Speed = ("+xSpeed+ ","+ ySpeed+")";
     }
-    public MovablePoint(){
+    public void moveUp(){
+        y -= ySpeed;
     }
-
-    public float getxSpeed() {
-        return xSpeed;
+    public void moveDown(){
+        y += ySpeed;
     }
-
-    public void setxSpeed(float xSpeed) {
-        this.xSpeed = xSpeed;
+    public void moveLeft(){
+        x -= xSpeed;
     }
-
-    public float getySpeed() {
-        return ySpeed;
-    }
-
-    public void setySpeed(float ySpeed) {
-        this.ySpeed = ySpeed;
-    }
-    public void setSpeed(float xSpeed, float ySpeed){
-        this.xSpeed = xSpeed;
-        this.ySpeed = ySpeed;
-    }
-    public float[] getSpeed(){
-        float getSpeed[] = {getxSpeed(), getySpeed()};
-        return getSpeed;
-    }
-
-    @Override
-    public String toString() {
-        return "("+getX()+","+getY()+"), speed = ("+xSpeed+","+ySpeed+")";
-    }
-    public MovablePoint move(){
-        setX(getX() + getxSpeed());
-        setY(getY()+ getySpeed());
-      return this;
+    public void moveRight(){
+        x += xSpeed;
     }
 }
